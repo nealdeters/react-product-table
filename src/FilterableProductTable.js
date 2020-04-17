3d70020d0be401401bfee53043ea020fb60e40b8
Neal Deters <nealdeters@gmail.com>
2020-04-17 14:58:30 -0500

    fixed filter to check against lowercase for name and filterText

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -40,7 +40,7 @@ class ProductTable extends React.Component {
     let lastCategory = null;
 
     this.props.products.forEach((product) => {
-      if (product.name.indexOf(filterText) === -1) {
+      if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
         return;
       }
       if (inStockOnly && !product.stocked) {
